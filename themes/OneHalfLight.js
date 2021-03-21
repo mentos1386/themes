386dc7f5bd7d6f8f418f8821d774560acba88f9d
Tine Jozelj <tine@tjo.space>
2021-03-21 11:54:47 +0100

    feat: create OneHalfLight theme
    source: https://github.com/sonph/onehalf/blob/master/wal/colorschemes/light/one-half-light.json

---
+++ themes/OneHalfLight.js
@@ -0,0 +1,21 @@
+t.prefs_.set('color-palette-overrides',[
+  "#383a42",
+  "#e45649",
+  "#50a14f",
+  "#c18401",
+  "#0184bc",
+  "#a626a4",
+  "#0997b3",
+  "#fafafa",
+  "#383a42",
+  "#e45649",
+  "#50a14f",
+  "#c18401",
+  "#0184bc",
+  "#a626a4",
+  "#0997b3",
+  "#fafafa"
+]);
+t.prefs_.set('foreground-color', "#383a42");
+t.prefs_.set('background-color', "#fafafa");
+t.prefs_.set('cursor-color', '#bfceff');
